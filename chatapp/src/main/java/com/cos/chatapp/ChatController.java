53fb6f9aa5fee4461cb536e0b21233348f779c24
donggeonL <vcvc7978@nate.com>
2022-04-16 00:46:24 +0900

    완성

--- chatapp/src/main/java/com/cos/chatapp/ChatController.java
+++ chatapp/src/main/java/com/cos/chatapp/ChatController.java
@@ -16,14 +16,24 @@ public class ChatController {
 
     private final ChatRepository chatRepository;
 
+    // 귓속말할때 사용
+    @CrossOrigin
     @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE) // produces = MediaType.TEXT_EVENT_STREAM_VALUE 데이터가 생길때마다 전송한다.
     public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver){
         return chatRepository.mFindBySender(sender,receiver).subscribeOn(Schedulers.boundedElastic());
     }
 
+    @CrossOrigin
+    @GetMapping(value = "/chat/roomNum/{roomNum}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> findByRoomNum(@PathVariable Integer roomNum) {
+        return chatRepository.mFindByRoomNum(roomNum)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @CrossOrigin
     @PostMapping("/chat")
     public Mono<Chat> setMsg(@RequestBody Chat chat){
         chat.setCreatedAt(LocalDateTime.now());
-        return chatRepository.save(chat);
+        return chatRepository.save(chat); // Object를 리턴하면 자동으로 JSON 변환 (MessageConverter)
     }
 }
